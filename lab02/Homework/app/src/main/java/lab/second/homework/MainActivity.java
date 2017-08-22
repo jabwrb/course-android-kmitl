50aeba79d508ce26617801cd324308a480a2c63d
Weerabhat Meelarp <s8070128@kmitl.ac.th>
2017-08-23 00:04:28 +0700

    Submit lab02 homework

---
+++ lab02/Homework/app/src/main/java/lab/second/homework/MainActivity.java
@@ -0,0 +1,13 @@
+package lab.second.homework;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homework_01);
+    }
+}
